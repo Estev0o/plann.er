32df036f5b63bcf55a713be60cf5045114ac038c
Vinicius Estevo <viniestevo33@gmail.com>
2024-07-10 20:35:11 -0300

    macete do EF para encontrar tebala

--- src/Journey.Infrastructure/JourneyDbContext.cs
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -11,5 +11,12 @@ namespace Journey.Infrastructure
         {
             optionsBuilder.UseSqlite("Data Source=C:\\Users\\vinip\\Music\\Journey\\JourneyDatabase.db");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Activity>().ToTable("Activities");
+        }
     }
 }
