5a88701053cce8ab2b7a8cf0da4765e3b18e3c4c
Vinicius Estevo <viniestevo33@gmail.com>
2024-07-11 21:03:31 -0300

    Nova execption NotFound

---
+++ src/Journey.Exception/ExceptionsBase/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using Journey.Exception.ExceptionsBase;
+
+public abstract class NotFoundException : JourneyException
+{
+    public NotFoundException(string message) : base(message)
+    {
+
+    }
+}
+
