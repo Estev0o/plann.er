9d7a274ce754a232d8fc992b32848958951f6318
Vinicius Estevo <viniestevo33@gmail.com>
2024-07-11 21:01:28 -0300

    Nova Exception

--- src/Journey.Application/UseCases/Trips/GetById/GetTripByIdUseCase.cs
+++ src/Journey.Application/UseCases/Trips/GetById/GetTripByIdUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using Journey.Communication.Responses;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
 using Journey.Infrastructure;
-using Journey.Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Journey.Application.UseCases.Trips.GetById
@@ -15,6 +16,11 @@ namespace Journey.Application.UseCases.Trips.GetById
                 .Trips
                 .Include(trip => trip.Activities)
                 .FirstOrDefault(trip => trip.Id == id);
+
+            if (trip is null)
+            {
+                throw new JourneyException(ResourceErrorMessages.TRIP_NOT_FOUND);
+            }
 
             return new ResponseTripJson
             {
